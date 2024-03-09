022b5e6dc3eaf22d04787be12592627f18cb4e0c
ApostolisNt <apo7ntask@gmail.com>
2024-03-09 11:59:18 +0200

    mongodb setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      xl: { max: "1280px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
     },
+    extend: {},
   },
   plugins: [],
 };
