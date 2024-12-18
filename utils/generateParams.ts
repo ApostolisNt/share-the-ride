e28322408849be4c66c1797b835fd33bbd3758b2
ApostolisNt <apo7ntask@gmail.com>
2024-12-18 13:32:41 +0200

    Keep calm and commit

--- utils/generateParams.ts
+++ utils/generateParams.ts
@@ -4,5 +4,9 @@ import { SupportedLangCodes } from "data/translations/translations";
 export const generateLanguageSlugs = () => {
   return general.languages.map((loc: string) => ({
     locale: loc as SupportedLangCodes,
+
+
+
+    
   }));
 };
