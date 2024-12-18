393dd3a7b75af82f97f37e69f17aad6eca1a955e
ApostolisNt <apo7ntask@gmail.com>
2024-12-18 12:54:02 +0200

    Keep calm and commit

--- utils/general.ts
+++ utils/general.ts
@@ -2,10 +2,9 @@ const DOUBLE_SLASH = /\/\//g;
 const HASH_SLASH = /#\/?$/g;
 const TRAILING_SLASH = /\/$/;
 
-
 export const cleanUrlSlash = (url: string) => {
+  return url
 
-    return url
     .replace(HASH_SLASH, "#") // Fixes: #/ -> #
     .replace(DOUBLE_SLASH, "/") // Fixes: // -> /
     .replace(TRAILING_SLASH, ""); // Fixes: / ->
